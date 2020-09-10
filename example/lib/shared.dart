8d1a5e78573e38c49fc86aaf6204dd2d4ebeb318
Salman S <salkuadrat@gmail.com>
2020-09-10 10:00:03 +0700

    refactor example project

--- example/lib/shared.dart
+++ example/lib/shared.dart
@@ -1,4 +1,3 @@
-import 'package:example/main.dart';
 import 'package:flutter/material.dart';
 
 enum DrawerType { slide, flip }
@@ -71,73 +70,4 @@ class DropdownCurve extends DropdownButton<Curve> {
     onChanged: onChanged,
     value: value,
   );
-}
-
-class MenuDrawer extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      child: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            stops: [0.0, 1.0], 
-            colors: [
-              Color(0xFF43CEA2),
-              Color(0xFF1D6DBD),
-            ],
-          ),
-        ),
-        child: SafeArea(
-          child: Theme(
-            data: ThemeData(brightness: Brightness.dark),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                if(type != DrawerType.slide) ListTile(
-                  leading: Icon(Icons.adjust),
-                  title: Text('Slide Drawer'),
-                  onTap: () {
-                    type = DrawerType.slide;
-                    App.of(context).restart();
-                  },
-                ),
-                if(type != DrawerType.flip) ListTile(
-                  leading: Icon(Icons.adjust),
-                  title: Text('Flip Drawer'),
-                  onTap: () {
-                    type = DrawerType.flip;
-                    App.of(context).restart();
-                  },
-                ),
-                ListTile(
-                  leading: Icon(Icons.rss_feed),
-                  title: Text('News'),
-                ),
-                ListTile(
-                  leading: Icon(Icons.favorite_border),
-                  title: Text('Favourites'),
-                ),
-                ListTile(
-                  leading: Icon(Icons.map),
-                  title: Text('Map'),
-                ),
-                ListTile(
-                  leading: Icon(Icons.settings),
-                  title: Text('Settings'),
-                ),
-                ListTile(
-                  leading: Icon(Icons.person_outline),
-                  title: Text('Profile'),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
 }
